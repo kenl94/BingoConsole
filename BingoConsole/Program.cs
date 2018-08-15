7d4244eb45faa5be534da0127ffc9b000453e436
Ken Ly <kly94@users.noreply.github.com>
2018-08-15 21:03:45 +0100

    Made loop to check if number exists

--- BingoConsole/Program.cs
+++ BingoConsole/Program.cs
@@ -10,6 +10,8 @@ namespace BingoConsole
     {
         static void Main(string[] args)
         {
+            List<int> RandomBingo = new List<int>();
+            Random num = new Random();
             // Find out the Users Name
             Console.WriteLine("What's your name?");
             string name = Console.ReadLine();
@@ -17,32 +19,41 @@ namespace BingoConsole
             Console.WriteLine("Welcome to Bingo " + name);
 
             // Random Number
-            Random num = new Random();
 
             Console.WriteLine("In this Bingo game, you only have to match 5 of the 10 numbers, choose numbers from 1 to 25");
 
-            string[] playersChoices = new string[5]; 
+            //string[,] playersChoices = new string[5,5];
+            //int[,] playersNummbers = Int32.Parse(playersChoices);
 
+            List<int> playerBingoCard = new List<int>() { 3, 11, 13, 17, 23 };
 
-
-            List<int> RandomBingo = new List<int>();
-
-            for (int i = 0; i < 10; i++)
+            while (RandomBingo.Count < 10)
             {
-                int rnd = num.Next(0, 25);
-                Console.WriteLine(rnd);
-                RandomBingo.Add(rnd);
-            }
-            
-            int[] RandomNumArray = RandomBingo.ToArray();
+                int rnd = num.Next(1, 25);
+                if (!RandomBingo.Contains(rnd))
+                {
+                    RandomBingo.Add(rnd);
+                    Console.WriteLine(rnd);
+                }
 
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine("1st number is " + RandomNumArray[i]);
             }
+            // If you want to see if the numbers are all different
+            RandomBingo.Sort();
+            for (int i = 0; i < RandomBingo.Count; i++)
+            {
+                Console.WriteLine("Number is "+ RandomBingo[i]);
+                for (int j = 0; j < playerBingoCard.Count; j++)
+                {
+                    if (RandomBingo[i] == playerBingoCard[j])
+                    {
+                    Console.WriteLine("Players number " + playerBingoCard[j]);
+                    }
+                }
+            }
+
+
 
             Console.ReadLine();
-
 
 
         }
