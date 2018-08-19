8dcc3d9198e164cd5e5e85907d8bbc59e20fd898
kenl94 <kly@spartaglobal.com>
2018-08-19 17:24:40 +0100

    Made a jagged array grid which gets random num like a bingo card 5x5

--- BingoConsole/Player.cs
+++ BingoConsole/Player.cs
@@ -12,35 +12,10 @@ namespace BingoConsolePlayers
 
         public int[] setOfNumbers = new int[] { 3, 7, 13, 15, 19 };
         public List<int> matchingNumbers;
+        public int[][] setOfNumbers2 = new int [5][];
 
 
     }
 }
-
-
-  
-  //public string player { get; set; }
-  //      public static Random playerRandomNum = new Random();
-  //      public static int[,] setOfNumbers = new int[5, 5];
-  //      public List<int> matchingNumbers;
-
-  //      public static void populateBingoCard()
-  //      {
-  //          int inum = 0;
-  //          while (inum < 100)
-  //          {
-
-  //              for (int i = 0; i < setOfNumbers.GetLength(0); i++)
-  //              {
-  //                  for (int j = 0; j < setOfNumbers.GetLength(1); j++)
-  //                  {
-  //              int setPlayerNums = playerRandomNum.Next(1, 75);
-  //                      setOfNumbers[i, j] = setPlayerNums;
-  //                  }
-  //              };
-
-  //              inum++;
-  //          }
-  //      }
  
  
--- BingoConsole/Program.cs
+++ BingoConsole/Program.cs
@@ -13,30 +13,44 @@ namespace BingoConsole
     {
         public static void Main()
         {
-            
+
 
             Player nameOfPlayer = new Player();
             WinCondition WinOrLose = new WinCondition();
-
+            Random number = new Random();
             Console.WriteLine("What's your name?");
             nameOfPlayer.player = Console.ReadLine();
 
             Console.WriteLine("Welcome to Bingo " + nameOfPlayer.player);
 
 
-                Console.WriteLine("In this Bingo game, you only have to match 5, when you're ready for the numbers press space!");
+            Console.WriteLine("In this Bingo game, you only have to match 5, when you're ready for the numbers press space!");
+            for (int i = 0; i < 5; i++)
+            {
+                nameOfPlayer.setOfNumbers2[i] = new int[5] { number.Next(1, 15), number.Next(16, 30), number.Next(31, 45), number.Next(46, 60), number.Next(61, 75) };
+
+                for (int j = 0; j < 5; j++)
+                {
+                    
+                    Console.Write(nameOfPlayer.setOfNumbers2[i][j] + ",");
+                }
+
+                Console.WriteLine();
+
+            }
+
 
             while (true)
             {
 
                 WinOrLose.Win();
-                
+
 
                 Console.WriteLine("Type [Y] To play again, otherwise any other key to exit");
 
-                string playagain = Console.ReadLine().ToLower();
+                string playagain = Console.ReadLine().ToUpper();
 
-                if (playagain == "y")
+                if (playagain == "Y")
                 {
                     Main();
                 }
